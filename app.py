1b5aeb112c584cd0cff587e9e6eb8dd9edd01aca
dfvasquez1504 <dani.vasquezdaz@gmail.com>
2025-10-16 13:53:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -34,11 +34,11 @@ st.title("MQTT Control")
 
 if st.button('ON'):
     act1="ON"
-    client1= paho.Client("GIT-HUB")                           
+    client1= paho.Client("Daniel_Client")                           
     client1.on_publish = on_publish                          
     client1.connect(broker,port)  
     message =json.dumps({"Act1":act1})
-    ret= client1.publish("daniel_on", message)
+    ret= client1.publish("daniel_luz", message)
  
     #client1.subscribe("Sensores")
     
@@ -48,11 +48,11 @@ else:
 
 if st.button('OFF'):
     act1="OFF"
-    client1= paho.Client("GIT-HUB")                           
+    client1= paho.Client("Daniel_Client")                           
     client1.on_publish = on_publish                          
     client1.connect(broker,port)  
     message =json.dumps({"Act1":act1})
-    ret= client1.publish("daniel_off", message)
+    ret= client1.publish("daniel_luz", message)
   
     
 else:
@@ -62,7 +62,7 @@ values = st.slider('Selecciona el rango de valores',0.0, 100.0)
 st.write('Values:', values)
 
 if st.button('Enviar valor analógico'):
-    client1= paho.Client("GIT-HUB")                           
+    client1= paho.Client("Daniel_Client")                           
     client1.on_publish = on_publish                          
     client1.connect(broker,port)   
     message =json.dumps({"Analog": float(values)})
